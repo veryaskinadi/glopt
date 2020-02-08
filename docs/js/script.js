f8bf1504053a2c7d70a730c4717f95bf7b6ae8f1
Dilyara <khakimovadi@gmail.com>
2020-02-08 17:01:30 +0300

    docs

--- docs/js/script.js
+++ docs/js/script.js
@@ -73,16 +73,14 @@ $(document).ready(function() {
             {
                 breakpoint: 768,
                 settings: {
-                    arrows: false,
                     centerMode: true,
                     centerPadding: '40px',
-                    slidesToShow: 3
+                    slidesToShow: 1
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
-                    arrows: false,
                     centerMode: true,
                     centerPadding: '40px',
                     slidesToShow: 1
--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ gulp.task('styles', function() {
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/css"))
-        .pipe(gulp.dest("dist/css"));
+        .pipe(gulp.dest("docs/css"));
 });
 
 gulp.task('watch', function() {
@@ -24,38 +24,38 @@ gulp.task('watch', function() {
 gulp.task('html', function() {
     return gulp.src("src/*.html")
         .pipe(htmlmin({collapseWhitespase: true }))
-        .pipe(gulp.dest("dist/"));
+        .pipe(gulp.dest("docs/"));
 });
 
 gulp.task('scripts', function() {
     return gulp.src("src/js/**/*.js")
-        .pipe(gulp.dest("dist/js"));
+        .pipe(gulp.dest("docs/js"));
 });
 
 gulp.task('fonts', function() {
     return gulp.src("src/fonts/**/*")
-        .pipe(gulp.dest("dist/fonts"));
+        .pipe(gulp.dest("docs/fonts"));
 });
 
 gulp.task('icons', function() {
     return gulp.src("src/icons/**/*")
-        .pipe(gulp.dest("dist/icons"));
+        .pipe(gulp.dest("docs/icons"));
 });
 
 gulp.task('mailer', function() {
     return gulp.src("src/mailer/**/*")
-        .pipe(gulp.dest("dist/mailer"));
+        .pipe(gulp.dest("docs/mailer"));
 });
 
 gulp.task('images', function() {
     return gulp.src("src/img/**/*")
         .pipe(imagemin())
-        .pipe(gulp.dest("dist/img"));
+        .pipe(gulp.dest("docs/img"));
 });
 
 gulp.task('favicon', function() {
     return gulp.src("src/*.ico")
-        .pipe(gulp.dest("dist/"));
+        .pipe(gulp.dest("docs/"));
 });
 
 gulp.task('default', gulp.parallel('watch', 'styles', 'scripts', 'html', 'fonts', 'icons', 'mailer', 'images', 'favicon'));
